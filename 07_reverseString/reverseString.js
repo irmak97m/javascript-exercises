4ad922f5a8caead1ebec3fe18da500c80f0afb58
irmak.97m <218309191+irmak97m@users.noreply.github.com>
2025-07-10 01:36:14 +0300

    reverseString done the longer way - check comments for shorter solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedStr ="";
+    let length = str.length; 
+    for ( i = length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
 };
+
+
+/**shorter way down below
+ * .split("") turns it into array
+ * .reverse() turns it upside down
+ * join("") get array members all 
+ * together without any space
+ */
+/*
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
