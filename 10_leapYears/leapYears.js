6f0f01c40d9b7ba6ce50cbdabc6abe8478dd1826
irmak.97m <218309191+irmak97m@users.noreply.github.com>
2025-07-11 14:14:22 +0300

    with and without arrow function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+/*const leapYears = function(year) {
+    if(year % 400 == 0 || (year % 4 == 0 && year % 100 !== 0)) {
+        return true;
+    } else { 
+        return false;
+    };
+};*/
 
-};
+//short version
+
+const leapYears = year => (year % 400 == 0 || (year % 4 == 0 && year % 100 !== 0)) ? true: false;
 
 // Do not edit below this line
 module.exports = leapYears;
