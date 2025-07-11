686a415086be6153cd770ee512f390bea7913b5e
irmak.97m <218309191+irmak97m@users.noreply.github.com>
2025-07-11 13:56:16 +0300

    array destructive and array methods

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+/** shorter method
+const removeFromArray = function(arr,...args) {
+    return arr.filter(val => !args.includes(val));
+};
+*/
+
+// longer method via creating new array
+
+const removeFromArray = function (arr,...args) {
+    const modedArray = [];
+    arr.forEach(val => {
+        if(!args.includes(val)) {
+            modedArray.push(val);
+        }
+    });
+    return modedArray;
 };
 
 // Do not edit below this line
