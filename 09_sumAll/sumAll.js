79199d7b8066054ea07dc9f935509925bd81e391
irmak.97m <218309191+irmak97m@users.noreply.github.com>
2025-07-11 13:54:44 +0300

    array destructing methods and array methods

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(first,second) {
+    if(first < 0 || first % 1 !== 0 || second < 0 || second % 1 !== 0 
+        || typeof(first) !== "number" || typeof(second) !== "number" ) {
+        return "ERROR";
+    } else {
+        let total = 0;
+        let small;
+        let big;
+        if (first > second) {
+            big = first;
+            small = second;
+        } else {
+            big = second;
+            small = first;
+        }
+        
+        for(i = small; i <= big; i++) {
+            total = total + i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
