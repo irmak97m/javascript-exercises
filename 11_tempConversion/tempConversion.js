0eb98a9880564a80d2778a2b650b3f88600dfdb7
irmak.97m <218309191+irmak97m@users.noreply.github.com>
2025-07-11 14:38:38 +0300

    math.round(val*10)/10 creates one decimal

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  degree = Math.round(((degree - 32) * 5 / 9) * 10) / 10;
+  return degree;
 };
 
-const convertToFahrenheit = function() {
+Math.round()
+
+const convertToFahrenheit = function(degree) {
+  degree = Math.round((degree * 9 / 5 + 32) * 10) / 10;
+  return degree;
 };
 
 // Do not edit below this line
